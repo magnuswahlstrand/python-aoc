5b73ee36bcafa5d24ce26f90af500c90064d8713
Magnus Wahlstrand <magnus.wahlstrand@gmail.com>
2021-12-01 09:02:25 +0100

    day 1: clean up

--- 2021/day-01/main.py
+++ 2021/day-01/main.py
@@ -1,22 +1,27 @@
 from util import read_input
 
 
-def count_increasing(input_file: str):
+def count_increasing(input_file: str, distance: int = 1):
     lines = read_input(input_file)
-    return sum([1 for (a, b) in zip(lines, lines[1:]) if int(b) > int(a)])
+    return sum(int(b) > int(a) for (a, b) in zip(lines, lines[distance:]))
 
-def count_increasing_average(input_file: str):
-    lines = read_input(input_file)
-    return sum([1 for (a, b) in zip(lines, lines[3:]) if int(b) > int(a)])
+
+def part_1():
+    return count_increasing("input.txt")
+
+
+def part_2():
+    return count_increasing("input.txt", 3)
 
 
 test_count = count_increasing("input_test.txt")
 assert test_count == 7
 print("result for day-1 test is:", test_count, "as expected")
-test_count = count_increasing_average("input_test.txt")
-assert test_count == 5
-print("result for day-2 test is:", test_count, "as expected")
 
-print("result for day-1:", count_increasing("input.txt"))
-print("result for day-2:", count_increasing_average("input.txt"))
+test_count2 = count_increasing("input_test.txt", 3)
+assert test_count2 == 5
+print("result for day-2 test is:", test_count2, "as expected")
 
+print()
+print("result for day-1:", part_1())
+print("result for day-2:", part_2())
