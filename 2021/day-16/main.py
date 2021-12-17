25f2e1e05bd08519a2ae169784db3bdc39ce4f94
Magnus Wahlstrand <magnus.wahlstrand@gmail.com>
2021-12-17 21:17:23 +0100

    2021-day 16: rewrite with StringIO

--- 2021/day-16/main.py
+++ 2021/day-16/main.py
@@ -1,28 +1,14 @@
 import binascii
+from io import StringIO
 from typing import List
 
 import numpy as np
 
 from util import read_lines, assert_test, read_input
 
-test_input = read_lines("input_test.txt")
-
-
-def print_bytes(bs):
-    print(to_bin(bs))
-
 
 def to_bin(bs):
     return bin(int.from_bytes(bs, "big"))[2:].zfill(len(bs) * 8)
-
-
-def read_bytes(bs: str, num_bytes, i):
-    return bs[i:i + num_bytes], i + num_bytes
-
-
-def read_bytes_int(bs: str, num_bytes, i):
-    read_bs, i = read_bytes(bs, num_bytes, i)
-    return int(read_bs, 2), i
 
 
 def op(ver: str, vals: List[int]):
@@ -44,56 +30,46 @@ def op(ver: str, vals: List[int]):
     raise ValueError("unexpected version", ver)
 
 
-def parse_packet(h):
-    i = 0
-    _ver, i = read_bytes_int(h, 3, i)
-    _type, i = read_bytes_int(h, 3, i)
+def parse_packet(h: StringIO):
+    _ver = int(h.read(3), 2)
+    _type = int(h.read(3), 2)
+    global ver_total
+    ver_total += _ver
 
     if _type == 4:
         val = ""
         while True:
-            prefix, i = read_bytes(h, 1, i)
-            v, i = read_bytes(h, 4, i)
-            val += v
+            prefix = h.read(1)
+            val += h.read(4)
             if prefix == '0':
-                return int(val, 2), i, _ver
+                return int(val, 2)
     else:
-        len_type, i = read_bytes(h, 1, i)
-        sum_ver = _ver
-        vals = []
-
+        len_type = h.read(1)
         if len_type == '0':
-            length, i = read_bytes_int(h, 15, i)
-            target = i + length
-            while i < target:
-                i, sum_ver, v = parse_packet_and_step(h, i, sum_ver)
+            length = int(h.read(15), 2)
+            target = h.tell() + length
+            vals = []
+            while h.tell() < target:
+                v = parse_packet(h)
                 vals.append(v)
         else:
-            length, i = read_bytes_int(h, 11, i)
-            for _ in range(length):
-                i, sum_ver, v = parse_packet_and_step(h, i, sum_ver)
-                vals.append(v)
+            length = int(h.read(11), 2)
+            vals = [parse_packet(h) for _ in range(length)]
 
-        return op(_type, vals), i, sum_ver
-
-
-def parse_packet_and_step(h, i, sum_ver):
-    v, j, _ver = parse_packet(h[i:])
-    i += j
-    sum_ver += _ver
-    return i, sum_ver, v
+        return op(_type, vals)
 
 
 def calc_sum_versions(input: str):
+    global ver_total
+    ver_total = 0
     h = to_bin(binascii.unhexlify(input))
-    val, i, vers = parse_packet(h)
-    return vers
+    parse_packet(StringIO(h))
+    return ver_total
 
 
 def calc_everything(input: str):
     h = to_bin(binascii.unhexlify(input))
-    val, i, vers = parse_packet(h)
-    return val
+    return parse_packet(StringIO(h))
 
 
 def part_1():
@@ -103,6 +79,8 @@ def part_1():
 def part_2():
     return calc_everything(read_input('input.txt'))
 
+
+test_input = read_lines("input_test.txt")
 
 assert to_bin(binascii.unhexlify("D2FE28")) == "110100101111111000101000"
 
@@ -120,6 +98,8 @@ assert_test(calc_everything("F600BC2D8F"), 0, 2)
 assert_test(calc_everything("9C005AC2F8F0"), 0, 2)
 assert_test(calc_everything("9C0141080250320F1802104A08"), 1, 2)
 
-# assert_test(foobar_v2(test_input), 230, 2)
 print("result for day-1:", part_1())
 print("result for day-2:", part_2())
+
+# Rewritten to use StringIO, inspired by mrphlip
+# https://github.com/mrphlip/aoc/blob/master/2021/16.py
